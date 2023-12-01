ab633882b0b828f67f0056f5d9b673b9b82cbda8
suman sharma <sumans@sumans-MacBook-Air.local>
2023-12-01 15:46:32 +0545

    initial

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 
 cleanFolder(); // clear temp folder on server start
 
-app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
